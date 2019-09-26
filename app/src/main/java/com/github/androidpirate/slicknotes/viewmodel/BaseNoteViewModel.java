15cc05af60a4dc0b3d3cbc4b6f9b3b32e4d4fe2c
androipirate <emrekalaycidesign@gmail.com>
2019-09-26 15:34:02 -0400

    New feat - Implement abstract BaseNoteViewModel class

---
+++ app/src/main/java/com/github/androidpirate/slicknotes/viewmodel/BaseNoteViewModel.java
@@ -0,0 +1,51 @@
+/*
+ * <!--
+ *  Copyright (C) 2016 The Android Open Source Project
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ * -->
+ */
+
+package com.github.androidpirate.slicknotes.viewmodel;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.github.androidpirate.slicknotes.data.Note;
+import com.github.androidpirate.slicknotes.repo.NoteRepository;
+
+public abstract class BaseNoteViewModel extends AndroidViewModel {
+    NoteRepository repo;
+    LiveData<Note> uiModel;
+    Note databaseModel;
+
+    BaseNoteViewModel(@NonNull Application application) {
+        super(application);
+        repo = new NoteRepository(application);
+    }
+
+    boolean checkDatabaseModelIsEmpty() {
+        return databaseModel.getTitle().isEmpty() && databaseModel.getDetails().isEmpty();
+    }
+
+    void displayEmptyNoteDiscardedToast() {
+        Toast.makeText(getApplication(),
+                "Empty note is discarded.",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+}
